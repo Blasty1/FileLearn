12d2b56b06aabcdd53222924a161b104e0b93bd6
Blasty1 <ichigoplayer18@gmail.com>
2020-06-25 11:44:55 +0200

    finish per ora

--- modules.mjs.js
+++ modules.mjs.js
@@ -42,82 +42,61 @@ class EventButtonWelcome{
 
 class Exercise{
     constructor(){
-
-    }
-    lista(){
-        <!DOCTYPE html>
-
+      <!DOCTYPE HTML>
+<html>
 <head>
   <meta charset="utf-8">
-  <link rel="stylesheet" href="style.css">
 </head>
-<style>
-  .gallery{
-    background-color:gray;
-    width:58%;
-   
-    border-radius:10px;
-    display:flex;
-    align-items:center;
-    overflow:hidden;
-    
-  }
-  .arrow{
-    height:50%;
-    flex:0
-  }
-  ul{
-    flex:1
-  }
-
-
-  </style>
 <body>
 
-
-<div class='gallery'>
-  <button class="arrow">⇦</button>
-  <ul>
-    <li><img src="https://en.js.cx/carousel/1.png"></li>
-    <li><img src="https://en.js.cx/carousel/2.png"></li>
-    <li><img src="https://en.js.cx/carousel/3.png"></li>
-    <li><img src="https://en.js.cx/carousel/4.png"></li>
-    <li><img src="https://en.js.cx/carousel/5.png"></li>
-    <li><img src="https://en.js.cx/carousel/6.png"></li>
-    <li><img src="https://en.js.cx/carousel/7.png"></li>
-    <li><img src="https://en.js.cx/carousel/8.png"></li>
-    <li><img src="https://en.js.cx/carousel/9.png"></li>
-    <li><img src="https://en.js.cx/carousel/10.png"></li>
+  <ul class="tree" id="tree">
+    <li>Animals
+      <ul>
+        <li>Mammals
+          <ul>
+            <li>Cows</li>
+            <li>Donkeys</li>
+            <li>Dogs</li>
+            <li>Tigers</li>
+          </ul>
+        </li>
+        <li>Other
+          <ul>
+            <li>Snakes</li>
+            <li>Birds</li>
+            <li>Lizards</li>
+          </ul>
+        </li>
+      </ul>
+    </li>
+    <li>Fishes
+      <ul>
+        <li>Aquarium
+          <ul>
+            <li>Guppy</li>
+            <li>Angelfish</li>
+          </ul>
+        </li>
+        <li>Sea
+          <ul>
+            <li>Sea trout</li>
+          </ul>
+        </li>
+      </ul>
+    </li>
   </ul>
-   <button class="arrow">⇨</button>
-  </div>
-
-
-  <script>
-    // label the images to visually track them, just for convenience,
-    // this code can be removed
-    let lis=document.querySelectorAll('li')
-    let arrowSinistra=document.querySelectorAll('.arrow')[0]
-    console.log(arrowSinistra)
-    arrowSinistra.addEventListener('click',test)
-    function test(){
-    let i=0;
-    for(let li of lis){
-      console.log(li.style.display)
-      if(li.style.display === 'none') break 
-     
-
-
-    }
-    }
-
-    // ...your code to make carousel alive!
-  </script>
 
 </body>
+<script>
+  tree.addEventListener('click',test)
+
+  function test(event){
+    if(event.target.localName === 'li') return;
+  }
+  </script>
 </html>
-https://plnkr.co/edit/qP4TJJCtiSTJf6Zi?p=preview&preview
     }
+  
     firstDiv(){
         const body=document.querySelector('body')
         console.log(body.children[1].lastElementChild)
